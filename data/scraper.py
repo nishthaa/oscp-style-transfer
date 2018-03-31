e4ce5945196bdf297014a2f1c18a50a870e72bad
Harshvardhan Kalra <harshvardhan14043@iiitd.ac.in>
2018-03-31 12:39:01 +0530

    Classifier working

---
+++ data/scraper.py
@@ -0,0 +1,74 @@
+import numpy as np
+import tweepy
+from tweepy.error import TweepError
+from tweepy import OAuthHandler
+
+
+def read(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        if (',') in lines[0]:
+            lines = map(lambda x: x.split(','), lines[1:])
+        else:
+            lines = map(lambda x: x.split(' - ')[::-1], lines)
+        content = map(lambda x: (x[0].strip(), x[-1].strip()), lines)
+    return content
+
+
+if __name__ == '__main__':
+    # Ideally, following should be hidden.
+    consumer_key = 'NaxHmBVQVAb8lf3qTd1DF1o2A'
+    consumer_secret = 'OCqQkczH3ZM51YYUlKXkcyIHXfWaPn98cKO1ALSeuVem4LNGV4'
+    access_token = '974376510583967746-uKEQ44qF5yr8PU1K2vcystnzouTxmH3'
+    access_token_secret = 'HewoZx4L15Ud9AugrHRUrIVhqgA4Eo9dvQ27cN4SuUfPG'
+
+    # Allow tweepy to access profile
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # Save all scraped tweets to one csv in the same form as twitter_gender.csv
+    # 0 for male, 1 for female, followed by tweet
+    save_csv = 'twitter_celeb_gender.csv'
+
+    for file in ('Top-1000-Celebrity-Twitter-Accounts.csv', 'bollywood.txt'):
+        content = read(file)
+        scraped_profiles_file = '{}_scraped.npy'.format(file.split('.')[0])
+        try:
+            scraped_profiles = np.load(scraped_profiles_file).item()
+        except IOError:
+            scraped_profiles = 0
+        for i, (celeb_handle, gender) in enumerate(content):
+            print i, celeb_handle, gender
+            if (i < scraped_profiles):
+                print 'Already scraped'
+                continue
+            all_tweets = []
+            oldest = None
+            while (True):
+                try:
+                    new_tweets = api.user_timeline(
+                        screen_name=celeb_handle,
+                        count=200,
+                        max_id=oldest,
+                        tweet_mode='extended')
+                except TweepError:
+                    break
+                if (len(new_tweets) == 0):
+                    break
+                all_tweets.extend(new_tweets)
+                oldest = all_tweets[-1].id - 1
+            tweets_to_save = map(
+                lambda x: x.full_text.encode('utf-8').replace('\n', ''),
+                all_tweets)
+            print 'Writing {} tweets to {}'\
+                .format(len(tweets_to_save), save_csv)
+            with open(save_csv, 'a') as f:
+                if (gender == 'F'):
+                    gender = 1
+                elif (gender == 'M'):
+                    gender = 0
+                for tweet in tweets_to_save:
+                    f.write('{},{}\n'.format(gender, tweet))
+            scraped_profiles += 1
+            np.save(scraped_profiles_file, scraped_profiles)
---
+++ models/prep_data.py
@@ -0,0 +1,105 @@
+import argparse
+from keras.preprocessing.text import Tokenizer
+import numpy as np
+import os
+import pdb
+import preprocessor
+
+parser = argparse.ArgumentParser(
+    description='Script for generating word one-hot vectors for model'
+)
+parser.add_argument(
+    '--save_tokenizer',
+    help='Save the tokenizer generated from all tweets',
+    default=False,
+    action='store_true'
+)
+parser.add_argument(
+    '--tweets_file',
+    help='Specify the ground truth file',
+    default=os.path.join(os.pardir, 'data', 'twitter_celeb_gender.csv'),
+    type=str
+)
+
+
+def read_tweets(filename):
+    from unidecode import unidecode
+    labels, tweets = [[] for _ in xrange(2)]
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            line = line.strip()
+            label, tweet = line.split(',', 1)
+            labels.append(int(label))
+            tweet = preprocessor.tokenize(tweet)
+            tweet = unidecode(tweet)
+            tweets.append(tweet)
+    return labels, tweets
+
+
+def fit_tokenizer(
+    tweets,
+    save_tokenizer,
+    tokenizer_save_filename='tokenizer.pickle'
+):
+    import cPickle as pickle
+    # pdb.set_trace()
+    if (not os.path.exists(tokenizer_save_filename)):
+        tokenizer = Tokenizer(num_words=5000)
+        print 'Fitting tokenizer on tweets'
+        tokenizer.fit_on_texts(tweets)
+        print 'Finished fitting tokenizer'
+        print 'Vocab size: {}'.format(len(tokenizer.word_counts))
+        if (save_tokenizer):
+            with open(tokenizer_save_filename, 'wb') as f:
+                pickle.dump(
+                    tokenizer,
+                    f,
+                    protocol=pickle.HIGHEST_PROTOCOL
+                )
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    tweets_GT_file = args.tweets_file
+    labels, tweets = read_tweets(tweets_GT_file)
+    fit_tokenizer(tweets, args.save_tokenizer)
+    rnd = np.random.get_state()
+    np.random.shuffle(tweets)
+    np.random.set_state(rnd)
+    np.random.shuffle(labels)
+
+    csv = tweets_GT_file.split(os.sep)[-1]
+    csv = csv.split('.')[0]
+    train_savefile = '{}_train'.format(csv)
+    test_savefile = '{}_test'.format(csv)
+    if (not os.path.exists('{}_tweets'.format(train_savefile))):
+        train_tweets, test_tweets = [[] for _ in xrange(2)]
+        train_labels, test_labels = [[] for _ in xrange(2)]
+        tweets_with_0, tweets_with_1 =\
+            filter(lambda x: x == 0, labels), filter(lambda x: x == 1, labels)
+        train_0_split_size, train_1_split_size =\
+            int(0.8 * len(tweets_with_0)), int(0.8 * len(tweets_with_1))
+        train_0_counter, train_1_counter = [0 for _ in xrange(2)]
+        # pdb.set_trace()
+        for i, tweet in enumerate(tweets):
+            if (labels[i] == 0):
+                if (train_0_counter < train_0_split_size):
+                    train_0_counter += 1
+                    train_tweets.append(tweet)
+                    train_labels.append(labels[i])
+                else:
+                    test_tweets.append(tweet)
+                    test_labels.append(labels[i])
+            elif (labels[i] == 1):
+                if (train_1_counter < train_1_split_size):
+                    train_1_counter += 1
+                    train_tweets.append(tweet)
+                    train_labels.append(labels[i])
+                else:
+                    test_tweets.append(tweet)
+                    test_labels.append(labels[i])
+        np.save('{}_tweets'.format(train_savefile), train_tweets)
+        np.save('{}_tweets'.format(test_savefile), test_tweets)
+        np.save('{}_labels'.format(train_savefile), train_labels)
+        np.save('{}_labels'.format(test_savefile), test_labels)
